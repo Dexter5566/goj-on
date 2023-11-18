b378d3229ebe5d05291d63b1d530f6cbafed24d0
Tommy Kao <tommy.kao@acer.com>
2023-11-18 16:58:50 +0800

    [mode] ok!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import { BlossomScene, myBlossomSceneConfig } from './sakura/sakura.component';
 
+interface Gojuon {
+    hiragana: string;
+    katakana: string;
+    romaji: string;
+    sign?: string;
+}
+
+const GOJUON: Gojuon[] = [
+    { hiragana: 'あ', katakana: 'ア', romaji: 'a' },
+    { hiragana: 'い', katakana: 'イ', romaji: 'i' },
+    { hiragana: 'う', katakana: 'ウ', romaji: 'u' },
+    { hiragana: 'え', katakana: 'エ', romaji: 'e' },
+    { hiragana: 'お', katakana: 'オ', romaji: 'o' },
+    { hiragana: 'か', katakana: 'カ', romaji: 'ka' },
+    { hiragana: 'き', katakana: 'キ', romaji: 'ki' },
+    { hiragana: 'く', katakana: 'ク', romaji: 'ku' },
+    { hiragana: 'け', katakana: 'ケ', romaji: 'ke' },
+    { hiragana: 'こ', katakana: 'コ', romaji: 'ko' },
+    { hiragana: 'さ', katakana: 'サ', romaji: 'sa' },
+    { hiragana: 'し', katakana: 'シ', romaji: 'shi' },
+    { hiragana: 'す', katakana: 'ス', romaji: 'su' },
+    { hiragana: 'せ', katakana: 'セ', romaji: 'se' },
+    { hiragana: 'そ', katakana: 'ソ', romaji: 'so' },
+    { hiragana: 'た', katakana: 'タ', romaji: 'ta' },
+    { hiragana: 'ち', katakana: 'チ', romaji: 'chi' },
+    { hiragana: 'つ', katakana: 'ツ', romaji: 'tsu' },
+    { hiragana: 'て', katakana: 'テ', romaji: 'te' },
+    { hiragana: 'と', katakana: 'ト', romaji: 'to' },
+    { hiragana: 'な', katakana: 'ナ', romaji: 'na' },
+    { hiragana: 'に', katakana: 'ニ', romaji: 'ni' },
+    { hiragana: 'ぬ', katakana: 'ヌ', romaji: 'nu' },
+    { hiragana: 'ね', katakana: 'ネ', romaji: 'ne' },
+    { hiragana: 'の', katakana: 'ノ', romaji: 'no' },
+    { hiragana: 'は', katakana: 'ハ', romaji: 'ha' },
+    { hiragana: 'ひ', katakana: 'ヒ', romaji: 'hi' },
+    { hiragana: 'ふ', katakana: 'フ', romaji: 'fu' },
+    { hiragana: 'へ', katakana: 'ヘ', romaji: 'he' },
+    { hiragana: 'ほ', katakana: 'ホ', romaji: 'ho' },
+    { hiragana: 'ま', katakana: 'マ', romaji: 'ma' },
+    { hiragana: 'み', katakana: 'ミ', romaji: 'mi' },
+    { hiragana: 'む', katakana: 'ム', romaji: 'mu' },
+    { hiragana: 'め', katakana: 'メ', romaji: 'me' },
+    { hiragana: 'も', katakana: 'モ', romaji: 'mo' },
+    { hiragana: 'や', katakana: 'ヤ', romaji: 'ya' },
+    { hiragana: 'ゆ', katakana: 'ユ', romaji: 'yu' },
+    { hiragana: 'よ', katakana: 'ヨ', romaji: 'yo' },
+    { hiragana: 'ら', katakana: 'ラ', romaji: 'ra' },
+    { hiragana: 'り', katakana: 'リ', romaji: 'ri' },
+    { hiragana: 'る', katakana: 'ル', romaji: 'ru' },
+    { hiragana: 'れ', katakana: 'レ', romaji: 're' },
+    { hiragana: 'ろ', katakana: 'ロ', romaji: 'ro' },
+    { hiragana: 'わ', katakana: 'ワ', romaji: 'wa' },
+    { hiragana: 'を', katakana: 'ヲ', romaji: 'wo' },
+    { hiragana: 'ん', katakana: 'ン', romaji: 'n' }
+];
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -15,56 +71,7 @@ export class AppComponent implements OnInit {
         wrong: new Audio('assets/music/wrong_1.mp3')
     };
 
-    gojuon = [
-        { hiragana: 'あ', katakana: 'ア', romaji: 'a' },
-        { hiragana: 'い', katakana: 'イ', romaji: 'i' },
-        { hiragana: 'う', katakana: 'ウ', romaji: 'u' },
-        { hiragana: 'え', katakana: 'エ', romaji: 'e' },
-        { hiragana: 'お', katakana: 'オ', romaji: 'o' },
-        { hiragana: 'か', katakana: 'カ', romaji: 'ka' },
-        { hiragana: 'き', katakana: 'キ', romaji: 'ki' },
-        { hiragana: 'く', katakana: 'ク', romaji: 'ku' },
-        { hiragana: 'け', katakana: 'ケ', romaji: 'ke' },
-        { hiragana: 'こ', katakana: 'コ', romaji: 'ko' },
-        { hiragana: 'さ', katakana: 'サ', romaji: 'sa' },
-        { hiragana: 'し', katakana: 'シ', romaji: 'shi' },
-        { hiragana: 'す', katakana: 'ス', romaji: 'su' },
-        { hiragana: 'せ', katakana: 'セ', romaji: 'se' },
-        { hiragana: 'そ', katakana: 'ソ', romaji: 'so' },
-        { hiragana: 'た', katakana: 'タ', romaji: 'ta' },
-        { hiragana: 'ち', katakana: 'チ', romaji: 'chi' },
-        { hiragana: 'つ', katakana: 'ツ', romaji: 'tsu' },
-        { hiragana: 'て', katakana: 'テ', romaji: 'te' },
-        { hiragana: 'と', katakana: 'ト', romaji: 'to' },
-        { hiragana: 'な', katakana: 'ナ', romaji: 'na' },
-        { hiragana: 'に', katakana: 'ニ', romaji: 'ni' },
-        { hiragana: 'ぬ', katakana: 'ヌ', romaji: 'nu' },
-        { hiragana: 'ね', katakana: 'ネ', romaji: 'ne' },
-        { hiragana: 'の', katakana: 'ノ', romaji: 'no' },
-        { hiragana: 'は', katakana: 'ハ', romaji: 'ha' },
-        { hiragana: 'ひ', katakana: 'ヒ', romaji: 'hi' },
-        { hiragana: 'ふ', katakana: 'フ', romaji: 'fu' },
-        { hiragana: 'へ', katakana: 'ヘ', romaji: 'he' },
-        { hiragana: 'ほ', katakana: 'ホ', romaji: 'ho' },
-        { hiragana: 'ま', katakana: 'マ', romaji: 'ma' },
-        { hiragana: 'み', katakana: 'ミ', romaji: 'mi' },
-        { hiragana: 'む', katakana: 'ム', romaji: 'mu' },
-        { hiragana: 'め', katakana: 'メ', romaji: 'me' },
-        { hiragana: 'も', katakana: 'モ', romaji: 'mo' },
-        { hiragana: 'や', katakana: 'ヤ', romaji: 'ya' },
-        { hiragana: 'ゆ', katakana: 'ユ', romaji: 'yu' },
-        { hiragana: 'よ', katakana: 'ヨ', romaji: 'yo' },
-        { hiragana: 'ら', katakana: 'ラ', romaji: 'ra' },
-        { hiragana: 'り', katakana: 'リ', romaji: 'ri' },
-        { hiragana: 'る', katakana: 'ル', romaji: 'ru' },
-        { hiragana: 'れ', katakana: 'レ', romaji: 're' },
-        { hiragana: 'ろ', katakana: 'ロ', romaji: 'ro' },
-        { hiragana: 'わ', katakana: 'ワ', romaji: 'wa' },
-        { hiragana: 'を', katakana: 'ヲ', romaji: 'wo' },
-        { hiragana: 'ん', katakana: 'ン', romaji: 'n' }
-    ];
-
-    mode: string = 'easy';
+    gojuon: Gojuon[] = [];
 
     cards: any = [];
 
@@ -102,33 +109,55 @@ export class AppComponent implements OnInit {
         }
     }
 
-    ngOnInit(): void {
+    menuItems: any = [
+        {
+            label: '平仮名/片仮名',
+            keys: ['hiragana', 'katakana']
+        },
+        {
+            label: '平仮名/羅馬',
+            keys: ['hiragana', 'romaji']
+        },
+        {
+            label: '片仮名/羅馬',
+            keys: ['katakana', 'romaji']
+        },
+        {
+            label: '平仮名/片仮名/羅馬',
+            keys: ['hiragana', 'katakana', 'romaji']
+        }
+    ];
+
+    selectMode(keys: (keyof Gojuon)[]): void {
+        this.gojuon = [...GOJUON];
+
         this.sounds.music.play();
 
+        const g1: Gojuon[] = [];
+        const g2: Gojuon[] = [];
+
+        this.gojuon.forEach(item => {
+            const randomIndex = Math.floor(Math.random() * keys.length);
+
+            let newKeys;
+
+            if (keys.length === 2) {
+                newKeys = keys;
+            } else {
+                newKeys = keys.slice(0, randomIndex).concat(keys.slice(randomIndex + 1));
+            }
+
+            g1.push({ ...item, sign: item[newKeys[0]] });
+            g2.push({ ...item, sign: item[newKeys[1]] });
+        });
+
+        this.shuffle(g1);
+        this.shuffle(g2);
+
+        this.cards = [...g1, ...g2];
+    }
+
+    ngOnInit(): void {
         new BlossomScene(myBlossomSceneConfig);
-
-        switch (this.mode) {
-            case 'easy':
-                const hiragana = this.gojuon.map(item => ({ ...item, sign: item.hiragana }));
-                const katakana = this.gojuon.map(item => ({ ...item, sign: item.katakana }));
-
-                this.shuffle(hiragana);
-                this.shuffle(katakana);
-
-                this.cards = [...hiragana, ...katakana];
-
-                break;
-            case 'mixed':
-                this.cards = [
-                    ...this.gojuon.map(item => ({ ...item, sign: item.hiragana })),
-                    ...this.gojuon.map(item => ({ ...item, sign: item.katakana }))
-                ];
-
-                this.shuffle(this.cards);
-                break;
-
-            default:
-                break;
-        }
     }
 }
